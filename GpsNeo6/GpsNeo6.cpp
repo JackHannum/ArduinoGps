1eaf1b3b4e95ea4e0530fa33e9ec93e88efaa087
Dalmir da Silva <dalmirdasilva@gmail.com>
2016-10-12 11:16:53 -0300

    add parser

--- GpsNeo6/GpsNeo6.cpp
+++ GpsNeo6/GpsNeo6.cpp
@@ -1,50 +1,292 @@
 #include <Arduino.h>
+#include <string.h>
+#include <stdlib.h>
 #include "GpsNeo6.h"
 
+GpsNeo6::GpsNeo6()
+        : fieldNumber(0), fieldOffset(0), parity(0), messageType(GPS_NEO6_MESSAGE_INVALID), isChecksumField(false), latitude(0), longitude(0), altitude(0), speed(0), fixType(NO_FIX), satellites(0), courseOverGround(0), verticalSpeedIndicator(0), horizontalDilutionOfPrecision(0), verticalDilutionOfPrecision(
+                0), year(0), month(0), day(0), hour(0), minute(0), second(0), locked(false) {
+}
+
+bool GpsNeo6::decode(unsigned char c) {
+    bool ok = false;
+    switch (c) {
+    case ',':
+        parity ^= c;
+        /* no break */
+    case '\r':
+    case '\n':
+    case '*':
+        ok = parseField();
+        isChecksumField = c == '*';
+        break;
+    case '$':
+        initializeParser();
+        break;
+    default:
+        if (fieldOffset < GPS_MEO6_MAX_FIELD_SIZE) {
+            field[fieldOffset++] = c;
+            parity ^= c;
+            ok = true;
+        }
+    }
+    return ok;
+}
+
+unsigned char GpsNeo6::decodeBytes(unsigned char *buf, unsigned char len) {
+    unsigned char c = len;
+    while (c-- > 0) {
+        if (!decode(*buf++)) {
+            return len - c;
+        }
+    }
+    return len;
+}
+
 double GpsNeo6::GpsNeo6::getLatitude() {
+    return latitude;
 }
 
 double GpsNeo6::GpsNeo6::getLongitude() {
+    return longitude;
 }
 
 double GpsNeo6::GpsNeo6::getAltitude() {
+    return altitude;
 }
 
 double GpsNeo6::GpsNeo6::getSpeed() {
+    return speed;
 }
 
 GpsNeo6::FixType GpsNeo6::getFixType() {
+    return fixType;
 }
 
 unsigned char GpsNeo6::getSatellites() {
+    return satellites;
 }
 
 double GpsNeo6::GpsNeo6::getCourseOverGround() {
+    return courseOverGround;
 }
 
 double GpsNeo6::GpsNeo6::getVerticalSpeedIndicator() {
+    return verticalSpeedIndicator;
 }
 
 double GpsNeo6::GpsNeo6::getHorizontalDilutionOfPrecision() {
+    return horizontalDilutionOfPrecision;
 }
 
 double GpsNeo6::GpsNeo6::getVerticalDilutionOfPrecision() {
+    return verticalDilutionOfPrecision;
 }
 
 unsigned char GpsNeo6::getYear() {
+    return year;
 }
 
 unsigned char GpsNeo6::getMonth() {
+    return month;
 }
 
 unsigned char GpsNeo6::getDay() {
+    return day;
 }
 
 unsigned char GpsNeo6::getHour() {
+    return hour;
 }
 
 unsigned char GpsNeo6::getMinute() {
+    return minute;
 }
 
 unsigned char GpsNeo6::getSecond() {
+    return second;
+}
+
+unsigned char GpsNeo6::isLocked() {
+    return locked;
+}
+
+void GpsNeo6::initializeParser() {
+    messageType = GPS_NEO6_MESSAGE_INVALID;
+    fieldNumber = 0;
+    fieldOffset = 0;
+    parity = 0;
+}
+
+bool GpsNeo6::parseField() {
+    bool isFieldValid = false;
+    if (fieldOffset < GPS_MEO6_MAX_FIELD_SIZE) {
+        field[fieldOffset] = 0;
+    }
+    if (fieldNumber == 0) {
+        isFieldValid = identifyMessage();
+    } else {
+        isFieldValid = parseIdentifiedMessage();
+    }
+    fieldNumber++;
+    fieldOffset = 0;
+    return isFieldValid;
+}
+
+bool GpsNeo6::isMessageValid() {
+    return true;
+}
+
+bool GpsNeo6::parseIdentifiedMessage() {
+    bool ok = false;
+    switch (messageType) {
+    case GPS_NEO6_MESSAGE_DTM:
+        ok = parseDatumReference();
+        break;
+    case GPS_NEO6_MESSAGE_GBS:
+        ok = parseSatelliteFaultDetection();
+        break;
+    case GPS_NEO6_MESSAGE_GGA:
+        ok = parsePositioningSystemFix();
+        break;
+    case GPS_NEO6_MESSAGE_GLL:
+        ok = parseLatitudeAndLongitude();
+        break;
+    case GPS_NEO6_MESSAGE_GPQ:
+        ok = parsePollMessage();
+        break;
+    case GPS_NEO6_MESSAGE_GRS:
+        ok = parseRangeResiduals();
+        break;
+    case GPS_NEO6_MESSAGE_GSA:
+        ok = parseDOPAndActiveSatellites();
+        break;
+    case GPS_NEO6_MESSAGE_GST:
+        ok = parsePseudoRangeErrorStatistics();
+        break;
+    case GPS_NEO6_MESSAGE_GSV:
+        ok = parseSatellitesInView();
+        break;
+    case GPS_NEO6_MESSAGE_RMC:
+        ok = parseRecommendedMinimumData();
+        break;
+    case GPS_NEO6_MESSAGE_TXT:
+        ok = parseTextTransmission();
+        break;
+    case GPS_NEO6_MESSAGE_VTG:
+        ok = parseCourseOverGroundAndGroundSpeed();
+        break;
+    case GPS_NEO6_MESSAGE_ZDA:
+        ok = parseTimeAndDate();
+        break;
+    }
+    return ok;
+}
+
+bool GpsNeo6::identifyMessage() {
+    messageType = GPS_NEO6_MESSAGE_INVALID;
+    if (strcmp((const char *) field, GPS_NEO6_MESSAGE_GBS_START) == 0) {
+        messageType = GPS_NEO6_MESSAGE_GBS;
+    } else if (strcmp((const char *) field, GPS_NEO6_MESSAGE_GGA_START) == 0) {
+        messageType = GPS_NEO6_MESSAGE_GGA;
+    } else if (strcmp((const char *) field, GPS_NEO6_MESSAGE_GLL_START) == 0) {
+        messageType = GPS_NEO6_MESSAGE_GLL;
+    } else if (strcmp((const char *) field, GPS_NEO6_MESSAGE_GPQ_START) == 0) {
+        messageType = GPS_NEO6_MESSAGE_GPQ;
+    } else if (strcmp((const char *) field, GPS_NEO6_MESSAGE_GRS_START) == 0) {
+        messageType = GPS_NEO6_MESSAGE_GRS;
+    } else if (strcmp((const char *) field, GPS_NEO6_MESSAGE_GSA_START) == 0) {
+        messageType = GPS_NEO6_MESSAGE_GSA;
+    } else if (strcmp((const char *) field, GPS_NEO6_MESSAGE_GST_START) == 0) {
+        messageType = GPS_NEO6_MESSAGE_GST;
+    } else if (strcmp((const char *) field, GPS_NEO6_MESSAGE_GSV_START) == 0) {
+        messageType = GPS_NEO6_MESSAGE_GSV;
+    } else if (strcmp((const char *) field, GPS_NEO6_MESSAGE_RMC_START) == 0) {
+        messageType = GPS_NEO6_MESSAGE_RMC;
+    } else if (strcmp((const char *) field, GPS_NEO6_MESSAGE_TXT_START) == 0) {
+        messageType = GPS_NEO6_MESSAGE_TXT;
+    } else if (strcmp((const char *) field, GPS_NEO6_MESSAGE_VTG_START) == 0) {
+        messageType = GPS_NEO6_MESSAGE_VTG;
+    } else if (strcmp((const char *) field, GPS_NEO6_MESSAGE_ZDA_START) == 0) {
+        messageType = GPS_NEO6_MESSAGE_ZDA;
+    } else if (strcmp((const char *) field, GPS_NEO6_MESSAGE_DTM_START) == 0) {
+        messageType = GPS_NEO6_MESSAGE_DTM;
+    }
+    return messageType != GPS_NEO6_MESSAGE_INVALID;
+}
+
+// $GPDTM,LLL,LSD,lat,N/S,lon,E/W,alt,RRR*cs<CR><LF>
+bool GpsNeo6::parseDatumReference() {
+    // Not implemented
+    return false;
+}
+
+bool GpsNeo6::parseSatelliteFaultDetection() {
+    // Not implemented
+    return false;
+}
+
+bool GpsNeo6::parsePositioningSystemFix() {
+    // Not implemented
+    return false;
+}
+
+// $GPGLL,4717.11634,N,00833.91297,E,124923.00,A,A*6E
+// $GPGLL,,,,,,V,N*64
+bool GpsNeo6::parseLatitudeAndLongitude() {
+    // Not implemented
+    return false;
+}
+
+bool GpsNeo6::parsePollMessage() {
+    // Not implemented
+    return false;
+}
+
+bool GpsNeo6::parseRangeResiduals() {
+    // Not implemented
+    return false;
+}
+
+bool GpsNeo6::parseDOPAndActiveSatellites() {
+    // Not implemented
+    return false;
+}
+
+bool GpsNeo6::parsePseudoRangeErrorStatistics() {
+    // Not implemented
+    return false;
+}
+
+// $GPGSV,2,1,05,19,,,22,24,,,22,25,,,22,26,,,23*73
+// $GPGSV,2,2,05,28,,,21*75
+bool GpsNeo6::parseSatellitesInView() {
+    bool ok = false;
+    switch (fieldNumber) {
+    case GPS_NEO6_MESSAGE_GSV_SATELLITES_IN_VIEW_FIELD:
+        satellites = (unsigned char) atoi((const char *) field);
+        ok = true;
+        break;
+    }
+    return ok;
+}
+
+bool GpsNeo6::parseRecommendedMinimumData() {
+    // Not implemented
+    return false;
+}
+
+bool GpsNeo6::parseTextTransmission() {
+    // Not implemented
+    return false;
+}
+
+bool GpsNeo6::parseCourseOverGroundAndGroundSpeed() {
+    // Not implemented
+    return false;
+}
+
+bool GpsNeo6::parseTimeAndDate() {
+    // Not implemented
+    return false;
 }
--- GpsNeo6/GpsNeo6.h
+++ GpsNeo6/GpsNeo6.h
@@ -11,10 +11,105 @@
 
 #include <Gps.h>
 
+#define GPS_MEO6_MAX_FIELD_SIZE 16
+
+#define GPS_NEO6_MESSAGE_GBS_START "GPGBS"
+#define GPS_NEO6_MESSAGE_GGA_START "GPGGA"
+#define GPS_NEO6_MESSAGE_GLL_START "GPGLL"
+#define GPS_NEO6_MESSAGE_GPQ_START "GPGPQ"
+#define GPS_NEO6_MESSAGE_GRS_START "GPGRS"
+#define GPS_NEO6_MESSAGE_GSA_START "GPGSA"
+#define GPS_NEO6_MESSAGE_GST_START "GPGST"
+#define GPS_NEO6_MESSAGE_GSV_START "GPGSV"
+#define GPS_NEO6_MESSAGE_RMC_START "GPRMC"
+#define GPS_NEO6_MESSAGE_TXT_START "GPTXT"
+#define GPS_NEO6_MESSAGE_VTG_START "GPVTG"
+#define GPS_NEO6_MESSAGE_ZDA_START "GPZDA"
+#define GPS_NEO6_MESSAGE_DTM_START "GPDTM"
+
+#define GPS_NEO6_MESSAGE_GSV_SATELLITES_IN_VIEW_FIELD   3
+
+#define JOIN(messageType, fieldNumber) (unsigned char)(((unsigned char)(messageType) << 4) | (fieldNumber & 0x0f))
+
 class GpsNeo6: public Gps {
+
+    unsigned char field[GPS_MEO6_MAX_FIELD_SIZE];
+    unsigned char fieldNumber;
+    unsigned char fieldOffset;
+    unsigned char parity;
+    unsigned char messageType;
+    bool isChecksumField;
+
+    double latitude;
+    double longitude;
+    double altitude;
+    double speed;
+    FixType fixType;
+    unsigned char satellites;
+    double courseOverGround;
+    double verticalSpeedIndicator;
+    double horizontalDilutionOfPrecision;
+    double verticalDilutionOfPrecision;
+    unsigned char year;
+    unsigned char month;
+    unsigned char day;
+    unsigned char hour;
+    unsigned char minute;
+    unsigned char second;
+    unsigned char locked;
 
 public:
 
+    enum MessageType {
+
+        // Datum Reference
+        GPS_NEO6_MESSAGE_INVALID = 0x00,
+
+        // GNSS Satellite Fault Detection
+        GPS_NEO6_MESSAGE_GBS = 0x01,
+
+        // Global positioning system fix data
+        GPS_NEO6_MESSAGE_GGA = 0x02,
+
+        // Latitude and longitude, with time of position fix and status
+        GPS_NEO6_MESSAGE_GLL = 0x03,
+
+        // Poll message
+        GPS_NEO6_MESSAGE_GPQ = 0x04,
+
+        // GNSS Range Residuals
+        GPS_NEO6_MESSAGE_GRS = 0x05,
+
+        // GNSS DOP and Active Satellites
+        GPS_NEO6_MESSAGE_GSA = 0x06,
+
+        // GNSS Pseudo Range Error Statistics
+        GPS_NEO6_MESSAGE_GST = 0x07,
+
+        // GNSS Satellites in View
+        GPS_NEO6_MESSAGE_GSV = 0x08,
+
+        // Recommended Minimum data
+        GPS_NEO6_MESSAGE_RMC = 0x09,
+
+        // Text Transmission
+        GPS_NEO6_MESSAGE_TXT = 0x0a,
+
+        // Course over ground and Ground speed
+        GPS_NEO6_MESSAGE_VTG = 0x0b,
+
+        // Time and Date
+        GPS_NEO6_MESSAGE_ZDA = 0x0c,
+
+        // Datum Reference
+        GPS_NEO6_MESSAGE_DTM = 0x0d
+    };
+
+    GpsNeo6();
+
+    bool decode(unsigned char c);
+
+    unsigned char decodeBytes(unsigned char *buf, unsigned char len);
 
     /**
      * Gps API.
@@ -35,6 +130,120 @@ public:
     unsigned char getHour();
     unsigned char getMinute();
     unsigned char getSecond();
+    unsigned char isLocked();
+
+private:
+
+    void initializeParser();
+    bool parseField();
+    bool isMessageValid();
+    bool parseIdentifiedMessage();
+    bool identifyMessage();
+
+    /**
+     * Datum Reference
+     * This message gives the difference between the currently selected Datum, and the reference Datum.
+     * If the currently configured Datum is not WGS84 or WGS72, then the field LLL will be set to 999,
+     * and the field LSD is set to a variable-length string, representing the Name of the Datum.
+     * The list of supported datums can be found in CFG-DAT.
+     * The reference Datum can not be changed and is always set to WGS
+     */
+    bool parseDatumReference();
+
+    /**
+     * GNSS Satellite Fault Detection
+     *
+     * This message outputs the results of the Receiver Autonomous Integrity Monitoring Algorithm (RAIM).
+     * - The fields errlat, errlon and erralt output the standard deviation of the position calculation,
+     * using all satellites which pass the RAIM test successfully.
+     * - The fields errlat, errlon and erralt are only output if the RAIM process passed successfully
+     * (i.e. no or successful Edits happened). These fields are never output if 4 or fewer satellites are used
+     * for the navigation calculation (because - in this case - integrity can not be determined by the receiver autonomously)
+     * - The fields prob, bias and stdev are only output if at least one satellite failed in the RAIM test.
+     * If more than one satellites fail the RAIM test, only the information for the worst satellite is output in this message.
+     */
+    bool parseSatelliteFaultDetection();
+
+    /**
+     * Global positioning system fix data
+     *
+     * The output of this message is dependent on the currently selected datum (Default: WGS84)
+     * Time and position, together with GPS fixing related data (number of satellites in use, and
+     * the resulting HDOP, age of differential data if in use, etc.).
+     */
+    bool parsePositioningSystemFix();
+
+    /**
+     * Latitude and longitude, with time of position fix and status
+     */
+    bool parseLatitudeAndLongitude();
+
+    /**
+     * Poll message
+     */
+    bool parsePollMessage();
+
+    /**
+     * GNSS Range Residuals
+     *
+     * This messages relates to associated GGA and GSA messages.
+     * If less than 12 SVs are available, the remaining fields are output empty. If more than 12 SVs are used,
+     * only the residuals of the first 12 SVs are output, in order to remain consistent with the NMEA standard.
+     */
+    bool parseRangeResiduals();
+
+    /**
+     * GNSS DOP and Active Satellites
+     *
+     * The GPS receiver operating mode, satellites used for navigation, and DOP values.
+     * - If less than 12 SVs are used for navigation, the remaining fields are left empty.
+     * If more than 12 SVs are used for navigation, only the IDs of the first 12 are output.
+     * - The SV Numbers (Fields 'Sv') are in the range of 1 to 32 for GPS satellites,
+     * and 33 to 64 for SBAS satellites (33 = SBAS PRN 120, 34 = SBAS PRN 121, and so on)
+     */
+    bool parseDOPAndActiveSatellites();
+
+    /**
+     * GNSS Pseudo Range Error Statistics
+     */
+    bool parsePseudoRangeErrorStatistics();
+
+    /**
+     * GNSS Satellites in View
+     *
+     * The number of satellites in view, together with each PRN (SV ID), elevation and azimuth,
+     * and C/No (Signal/Noise Ratio) value. Only four satellite details are transmitted in one message.
+     */
+    bool parseSatellitesInView();
+
+    /**
+     * Recommended Minimum data
+     *
+     * The output of this message is dependent on the currently selected datum (Default: WGS84)
+     * The Recommended Minimum sentence defined by NMEA for GPS/Transit system data.
+     */
+    bool parseRecommendedMinimumData();
+
+    /**
+     * Text Transmission
+     *
+     * This message is not configured through CFG-MSG, but instead through CFG-INF.
+     * This message outputs various information on the receiver, such as power-up screen,
+     * software version etc. This message can be configured using UBX Protocol message CFG-INF
+     */
+    bool parseTextTransmission();
+
+    /**
+     * Course over ground and Ground speed
+     *
+     * Velocity is given as Course over Ground (COG) and Speed over Ground (SOG).
+     */
+    bool parseCourseOverGroundAndGroundSpeed();
+
+    /**
+     * Time and Date
+     */
+    bool parseTimeAndDate();
 };
 
 #endif /* __ARDUINO_DRIVER_GPS_NEO6_H__ */
